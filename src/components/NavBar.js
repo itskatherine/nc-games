1fb6606ec4e0fa0b8cd0878acdb7f762a7920cf1
itskatherine <hurstkatherine@hotmail.co.uk>
2022-06-14 19:41:27 +0100

    Added burger menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,29 @@
 import { Link } from "react-router-dom";
+import Hamburger from "hamburger-react";
+import { useState } from "react";
 
 const NavBar = ({ categories }) => {
+  const [isOpen, setOpen] = useState(false);
+
   return (
     <>
-      <Link to="/">
-        <h2>Home</h2>
-      </Link>
-      <h2>Categories</h2>
-      <ul>
-        {categories.map((category, index) => {
-          return (
-            <li key={index}>
-              <Link to={`/reviews/categories/${category.slug}`}>
-                {category.slug}
-              </Link>
-            </li>
-          );
-        })}
+      <Hamburger toggled={isOpen} toggle={setOpen} />
+      <ul className={`burger-menu ${isOpen ? " showMenu" : ""}`}>
+        <Link to="/">
+          <h2>Home</h2>
+        </Link>
+        <h2>Categories</h2>
+        <ul>
+          {categories.map((category, index) => {
+            return (
+              <li key={index}>
+                <Link to={`/reviews/categories/${category.slug}`}>
+                  {category.slug}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
       </ul>
     </>
   );
